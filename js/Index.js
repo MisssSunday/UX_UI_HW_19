9ecf234790d250ec41a139c5516006908cd6cb38
MisssSunday <missyannini@gmail.com>
2021-03-22 16:23:23 -0400

    new update

--- js/Index.js
+++ js/Index.js
@@ -1,5 +1,19 @@
+console.log("Your index.js file is loaded correctly")
 
+//method1
 
+$(image2).ready(function(){
+  $("button").click(function(){
+    $("div").animate({
+      left: '250px',
+      opacity: '0.5',
+      height: '40px',
+      width: '40px'
+    });
+  });
+});
+
+//method2
 
 $("circle").click( function(){
     $('.image2').animate( {left: '250px'}, 1000 );
